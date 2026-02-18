9bdf53d07de68e3afc01a59bd5e91ffd146269f6
ldev <ldev@ldev.eu.org>
2026-02-18 19:01:32 +0100

    added start end len and Into<Range<usize>> for Span

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,16 @@ impl Span {
         Self(0..str.len())
     }
 
+    pub fn start(&self) -> usize {
+        self.0.start
+    }
+    pub fn end(&self) -> usize {
+        self.0.end
+    }
+    pub fn len(&self) -> usize {
+        self.0.end - self.0.start
+    }
+
     pub fn split(&mut self, index: usize) -> (Self, Self) {
         let index = self.0.start + index;
         ((self.0.start..index).into(), (index..self.0.end).into())
@@ -23,7 +33,12 @@ impl From<Range<usize>> for Span {
 
 impl Into<SourceSpan> for Span {
     fn into(self) -> SourceSpan {
-        (self.0.start, self.0.end - self.0.start).into()
+        (self.start(), self.len()).into()
+    }
+}
+impl Into<Range<usize>> for Span {
+    fn into(self) -> Range<usize> {
+        self.0
     }
 }
 
